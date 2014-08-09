d4e7f0c6c0a6492e767b377aa763f393ef37b63a
Izaak Meckler <ihmeckler@gmail.com>
2014-08-08 21:23:23 -0500

    added string splitting functions

--- src/core_string.ml
+++ src/core_string.ml
@@ -1,5 +1,82 @@
-include String
+include StringLabels
 
+let lsplit2_exn line ~on:delim =
+  let pos = StringLabels.index line delim in
+  (sub line ~pos:0 ~len:pos,
+   sub line ~pos:(pos+1) ~len:(String.length line - pos - 1)
+  )
+
+let rsplit2_exn line ~on:delim =
+  let pos = StringLabels.rindex line delim in
+  (sub line ~pos:0 ~len:pos,
+   sub line ~pos:(pos+1) ~len:(String.length line - pos - 1)
+  )
+
+let lsplit2 line ~on =
+  try Some (lsplit2_exn line ~on) with Not_found -> None
+
+let rsplit2 line ~on =
+  try Some (rsplit2_exn line ~on) with Not_found -> None
+
+let rec char_list_mem l (c:char) =
+  match l with
+  | [] -> false
+  | hd::tl -> hd = c || char_list_mem tl c
+
+let split_gen str ~on =
+  let is_delim =
+    match on with
+    | `char c' -> (fun c -> c = c')
+    | `char_list l -> (fun c -> char_list_mem l c)
+  in
+  let len = String.length str in
+  let rec loop acc last_pos pos =
+    if pos = -1 then
+      sub str ~pos:0 ~len:last_pos :: acc
+    else
+      if is_delim str.[pos] then
+        let pos1 = pos + 1 in
+        let sub_str = sub str ~pos:pos1 ~len:(last_pos - pos1) in
+        loop (sub_str :: acc) pos (pos - 1)
+    else loop acc last_pos (pos - 1)
+  in
+  loop [] len (len - 1)
+;;
+
+let split str ~on = split_gen str ~on:(`char on) ;;
+
+let split_on_chars str ~on:chars =
+  split_gen str ~on:(`char_list chars)
+;;
+
+let split_lines =
+  let back_up_at_newline ~t ~pos ~eol =
+    pos := !pos - (if !pos > 0 && t.[!pos - 1] = '\r' then 2 else 1);
+    eol := !pos + 1;
+  in
+  fun t ->
+    let n = length t in
+    if n = 0
+    then []
+    else
+      (* Invariant: [-1 <= pos < eol]. *)
+      let pos = ref (n - 1) in
+      let eol = ref n in
+      let ac = ref [] in
+      (* We treat the end of the string specially, because if the string ends with a
+         newline, we don't want an extra empty string at the end of the output. *)
+      if t.[!pos] = '\n' then back_up_at_newline ~t ~pos ~eol;
+      while !pos >= 0 do
+        if t.[!pos] <> '\n'
+        then decr pos
+        else
+          (* Becuase [pos < eol], we know that [start <= eol]. *)
+          let start = !pos + 1 in
+          ac := sub t ~pos:start ~len:(!eol - start) :: !ac;
+          back_up_at_newline ~t ~pos ~eol
+      done;
+      sub t ~pos:0 ~len:!eol :: !ac
+;;
 let concat ?(sep="") ts = concat sep ts
 
 let concat_array ?(sep="") ts =
@@ -13,3 +90,4 @@ let init n ~f =
   done;
   t
 ;;
+
