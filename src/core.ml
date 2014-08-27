654a86140b72aa11422b8b927303c478d4754990
Izaak Meckler <ihmeckler@gmail.com>
2014-08-27 14:17:07 -0500

    amended the defn of print as per js_of_ocaml's warning

--- src/core.ml
+++ src/core.ml
@@ -37,7 +37,7 @@ let set_global s x =
   Js.Unsafe.(set (variable "window") (Js.string s) x)
 
 let print x : unit = 
-  Js.Unsafe.(fun_call (variable "console.log") [| inject x |])
+  (Js.Unsafe.variable "console")##log(x)
 
 let println s : unit = print (Js.string s)
 
